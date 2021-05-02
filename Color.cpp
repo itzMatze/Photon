6fdacb34cfb5f5584a6fb64109e3e93c16f79815
itzMatze <50420199+itzMatze@users.noreply.github.com>
2021-05-02 09:54:36 +0200

    Fixed wrong casting of colors

--- Color.cpp
+++ Color.cpp
@@ -18,12 +18,12 @@ Color::Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a)
 
 unsigned int Color::get_hex_color() const
 {
-    unsigned int color = r > 1.0f ? 255 : static_cast<unsigned int>(r) * 255;
+    unsigned int color = r > 1.0f ? 255 : static_cast<unsigned int>(r * 255);
     color <<= 8;
-    color += g > 1.0f ? 255 : static_cast<unsigned int>(g) * 255;
+    color += g > 1.0f ? 255 : static_cast<unsigned int>(g * 255);
     color <<= 8;
-    color += b > 1.0f ? 255 : static_cast<unsigned int>(b) * 255;
+    color += b > 1.0f ? 255 : static_cast<unsigned int>(b * 255);
     color <<= 8;
-    color += a > 1.0f ? 255 : static_cast<unsigned int>(a) * 255;
+    color += a > 1.0f ? 255 : static_cast<unsigned int>(a * 255);
     return color;
 }
