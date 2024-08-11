77d23a7835824d95ab046984d71f98409b8e3685
Matthias Bruns <matthiasbruns35@gmail.com>
2024-08-11 21:59:58 +0200

    fix rendering buckets computation

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -33,23 +33,23 @@ void Renderer::init(SceneFile& scene_file, const std::string& name, const Settin
   buckets.clear();
 
   // divide image into buckets that can be rendered concurrently
-  const glm::uvec2 bucket_count = (scene_file.settings.resolution / scene_file.settings.bucket_size);
   const glm::uvec2 bucket_size = glm::uvec2(scene_file.settings.bucket_size, scene_file.settings.bucket_size);
+  const glm::uvec2 bucket_count = (scene_file.settings.resolution / bucket_size);
   // add one overflow bucket if the buckets do not fit the resolution
-  const glm::uvec2 overflow_bucket_size = glm::uvec2(scene_file.settings.resolution.x % scene_file.settings.bucket_size, scene_file.settings.resolution.y % scene_file.settings.bucket_size);
+  const glm::uvec2 overflow_bucket_size = glm::uvec2(scene_file.settings.resolution.x % bucket_size.x, scene_file.settings.resolution.y % bucket_size.y);
   for (uint32_t x = 0; x < bucket_count.x; x++)
   {
     for (uint32_t y = 0; y < bucket_count.y; y++)
     {
       buckets.emplace_back(bucket_size * glm::uvec2(x, y), bucket_size * glm::uvec2(x + 1, y + 1));
     }
-    if (overflow_bucket_size.x > 0) buckets.emplace_back(bucket_size * glm::uvec2(x, bucket_count.y), bucket_size * glm::uvec2(x + 1, bucket_count.y + overflow_bucket_size.y));
+    if (overflow_bucket_size.y > 0) buckets.emplace_back(bucket_size * glm::uvec2(x, bucket_count.y), bucket_size * glm::uvec2(x + 1, bucket_count.y) + glm::uvec2(0, overflow_bucket_size.y));
   }
-  if (overflow_bucket_size.y > 0)
+  if (overflow_bucket_size.x > 0)
   {
     for (uint32_t y = 0; y < bucket_count.y; y++)
     {
-      buckets.emplace_back(bucket_size * glm::uvec2(bucket_count.x, y), bucket_size * glm::uvec2(bucket_count.x + overflow_bucket_size.x, y));
+      buckets.emplace_back(bucket_size * glm::uvec2(bucket_count.x, y), bucket_size * glm::uvec2(bucket_count.x, y + 1) + glm::uvec2(overflow_bucket_size.x, 0));
     }
   }
 }
