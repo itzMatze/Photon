06639f88e76dcc54113ad2e54cfbcc7d6a9ebacc
Matthias Bruns <matthiasbruns35@gmail.com>
2024-08-14 15:12:33 +0200

    store bvh indices in global vector of bvh to improve traversation performance

--- include/object/bvh.hpp
+++ include/object/bvh.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
 #include <numeric>
 #include <stack>
@@ -26,23 +27,23 @@ public:
     {
       const Node& node = nodes[nodes_to_test.top()];
       nodes_to_test.pop();
-      if (!node.is_leaf && node.bounding_box.intersect(ray))
-      {
-        nodes_to_test.push(node.children.left);
-        nodes_to_test.push(node.children.right);
-      }
-      else
+      if (node.is_leaf)
       {
         // if node is a leaf test the contained objects
-        for (const uint32_t idx : node.indices)
+        for (uint32_t i = node.indices_offset; i < node.indices_offset + node.indices_count; i++)
         {
-          if (objects[idx].intersect(ray, cur_hit_info) && cur_hit_info.t < hit_info.t)
+          if (objects[indices[i]].intersect(ray, cur_hit_info) && cur_hit_info.t < hit_info.t)
           {
             hit_info = cur_hit_info;
-            hit_info.object_id = idx;
+            hit_info.object_id = indices[i];
             if (ray.config.anyhit) return true;
           }
         }
+      }
+      else if (node.bounding_box.intersect(ray))
+      {
+        if (node.children.left != -1) nodes_to_test.push(node.children.left);
+        if (node.children.right != -1) nodes_to_test.push(node.children.right);
       }
     }
     return hit_info.t < previous_closest;
@@ -51,23 +52,30 @@ public:
 private:
   struct Node
   {
-    Node(const std::vector<uint32_t>& indices, const std::vector<T>& objects);
+    Node(int32_t indices_offset, int32_t indices_count, const std::vector<T>& objects, const std::vector<uint32_t>& indices);
 
     AABB bounding_box;
     bool is_leaf;
-    struct
+    union
     {
-      int32_t left;
-      int32_t right;
-    } children;
-    std::vector<uint32_t> indices;
+      struct
+      {
+        int32_t left;
+        int32_t right;
+      } children;
+      struct
+      {
+        int32_t indices_offset;
+        int32_t indices_count;
+      };
+    };
   };
 
   void init(const std::vector<T>& objects, uint32_t threshold)
   {
-    std::vector<uint32_t> indices(objects.size());
+    indices = std::vector<uint32_t>(objects.size());
     std::iota(indices.begin(), indices.end(), 0);
-    nodes.push_back(BVH<T>::Node(indices, objects));
+    nodes.push_back(BVH<T>::Node(0, objects.size(), objects, indices));
     bool done = false;
     uint32_t axis = 0;
     // split nodes until termination criterium is fullfilled
@@ -78,85 +86,94 @@ private:
       uint32_t node_count = nodes.size();
       for (uint32_t i = 0; i < node_count; i++)
       {
-        Node& node = nodes[i];
         // every node must contain no more than `threshold` many elements
-        if (node.indices.size() > threshold)
+        if (nodes[i].is_leaf && nodes[i].indices_count > threshold)
         {
-          if (split(node, axis, objects)) done = false;
+          if (split(i, axis, objects)) done = false;
         }
       }
       axis = (axis + 1) % 3;
     }
   }
 
-  void apply_split(BVH<T>::Node& node, const std::vector<T>& objects, std::vector<uint32_t>& indices_0, std::vector<uint32_t>& indices_1, std::vector<uint32_t>& common_indices)
+  void apply_split(uint32_t node_idx, const std::vector<T>& objects, std::vector<int8_t>& classifications, uint32_t child_0_count, uint32_t child_1_count, uint32_t common_count)
   {
+    const uint32_t total_indices_count = nodes[node_idx].indices_count;
     // distribute the objects as evenly as possible to the children
-    int32_t diff = indices_0.size() - indices_1.size();
-    uint32_t common_idx = 0;
-    // fill up child with fewer elements
-    while (diff > 0 && common_idx < common_indices.size())
-    {
-      indices_1.push_back(common_indices[common_idx]);
-      diff--;
-      common_idx++;
-    }
-    while (diff < 0 && common_idx < common_indices.size())
-    {
-      indices_0.push_back(common_indices[common_idx]);
-      diff++;
-      common_idx++;
-    }
-    // distribute the remaining objects alternatingly
-    while (common_idx < common_indices.size())
-    {
-      if (diff & 1u) indices_0.push_back(common_indices[common_idx]);
-      else indices_1.push_back(common_indices[common_idx]);
-      diff++;
-      common_idx++;
-    }
+    // sort indices into buckets: first bucket contains child 0 indices, second common indices, and third child 1 indices
+    const uint32_t bucket_offsets[3] = {0, child_0_count, child_0_count + common_count};
+    uint32_t bucket_indices[3] = {bucket_offsets[0], bucket_offsets[1], bucket_offsets[2]};
+    // sort into buckets using auxiliary vector
+    std::vector<uint32_t> auxiliary(total_indices_count);
+    for (uint32_t i = 0; i < total_indices_count; i++) auxiliary[bucket_indices[classifications[i]]++] = indices[nodes[node_idx].indices_offset + i];
+    // copy back result
+    for (uint32_t i = 0; i < total_indices_count; i++) indices[nodes[node_idx].indices_offset + i] = auxiliary[i];
+    // split indices evenly and limit splitting point to bucket containing common indices
+    const uint32_t split_idx = std::clamp(total_indices_count / 2, bucket_offsets[1], bucket_offsets[2]);
+    const uint32_t indices_offset = nodes[node_idx].indices_offset;
+    nodes[node_idx].is_leaf = false;
+    nodes[node_idx].children.left = -1;
+    nodes[node_idx].children.right = -1;
     // create child nodes, their aabb is computed in the constructor
-    node.is_leaf = false;
-    node.indices.clear();
-    node.children.left = nodes.size();
-    node.children.right = nodes.size() + 1;
-    if (indices_0.size() > 0) nodes.emplace_back(indices_0, objects);
-    if (indices_1.size() > 0) nodes.emplace_back(indices_1, objects);
+    if (split_idx > 0)
+    {
+      nodes[node_idx].children.left = nodes.size();
+      nodes.emplace_back(indices_offset, split_idx, objects, indices);
+    }
+    if (split_idx < total_indices_count)
+    {
+      nodes[node_idx].children.right = nodes.size();
+      nodes.emplace_back(indices_offset + split_idx, total_indices_count - split_idx, objects, indices);
+    }
   }
 
-  bool split(BVH<T>::Node& node, uint32_t axis, const std::vector<T>& objects)
+  bool split(uint32_t node_idx, uint32_t axis, const std::vector<T>& objects)
   {
-    // store which objects are contained in which child and which objects are contained in both
-    std::vector<uint32_t> indices_0;
-    std::vector<uint32_t> indices_1;
-    std::vector<uint32_t> common_indices;
     // test all axis until one is found that is able to separate some objects
     // prevent useless nodes and enable detection of endless computation
     // due to all objects being put in the same node all the time caused by floating point errors
     for (uint32_t i = 0; i < 3; i++)
     {
-      indices_0.clear();
-      indices_1.clear();
-      common_indices.clear();
+      // store which objects are contained in which child and which objects are contained in both
+      // use child_0: 0; common: 1; child_1: 2 for easy indexing when sorting into buckets
+      std::vector<int8_t> classifications(nodes[node_idx].indices_count, -1);
+      uint32_t child_0_count = 0;
+      uint32_t child_1_count = 0;
+      uint32_t common_count = 0;
       // split bounding box into two new ones at the specified axis
-      float mid = (node.bounding_box.max[axis] - node.bounding_box.min[axis]) / 2.0;
-      glm::vec3 p0 = node.bounding_box.min;
+      float mid = (nodes[node_idx].bounding_box.max[axis] - nodes[node_idx].bounding_box.min[axis]) / 2.0;
+      glm::vec3 p0 = nodes[node_idx].bounding_box.min;
       p0[axis] += mid;
-      glm::vec3 p1 = node.bounding_box.max;
+      glm::vec3 p1 = nodes[node_idx].bounding_box.max;
       p1[axis] -= mid;
-      AABB box_0(node.bounding_box.min, p1);
-      AABB box_1(p0, node.bounding_box.max);
-      for (uint32_t i : node.indices)
+      AABB box_0(nodes[node_idx].bounding_box.min, p1);
+      AABB box_1(p0, nodes[node_idx].bounding_box.max);
+      for (uint32_t i = 0; i < nodes[node_idx].indices_count; i++)
       {
-        bool intersect_0 = objects[i].intersect(box_0);
-        bool intersect_1 = objects[i].intersect(box_1);
-        if (intersect_0 && intersect_1) common_indices.push_back(i);
-        else if (intersect_0) indices_0.push_back(i);
-        else if (intersect_1) indices_1.push_back(i);
+        const uint32_t object_idx = indices[nodes[node_idx].indices_offset + i];
+        const bool intersect_0 = objects[object_idx].intersect(box_0);
+        const bool intersect_1 = objects[object_idx].intersect(box_1);
+        if (intersect_0 && intersect_1)
+        {
+          classifications[i] = 1;
+          common_count++;
+        }
+        else if (intersect_0)
+        {
+          classifications[i] = 0;
+          child_0_count++;
+        }
+        else if (intersect_1)
+        {
+          classifications[i] = 2;
+          child_1_count++;
+        }
       }
-      if ((indices_0.size() > 0 && indices_1.size() > 0) || common_indices.size() > 0)
+      assert(classifications.size() == child_0_count + child_1_count + common_count);
+      assert(classifications.size() == nodes[node_idx].indices_count);
+      if ((child_0_count > 0 && child_1_count > 0) || common_count > 0)
       {
-        apply_split(node, objects, indices_0, indices_1, common_indices);
+        apply_split(node_idx, objects, classifications, child_0_count, child_1_count, common_count);
         return true;
       }
       axis = (axis + 1) % 3;
@@ -165,4 +182,5 @@ private:
   }
 
   std::vector<Node> nodes;
+  std::vector<uint32_t> indices;
 };
--- src/object/bvh.cpp
+++ src/object/bvh.cpp
@@ -10,12 +10,13 @@ template<> BVH<ObjectInstance>::BVH(const std::vector<ObjectInstance>& objects, 
   spdlog::debug("Successfully created TLAS with {} nodes", nodes.size());
 }
 
-template<> BVH<ObjectInstance>::Node::Node(const std::vector<uint32_t>& indices, const std::vector<ObjectInstance>& object_instances) : is_leaf(true), indices(indices)
+template<> BVH<ObjectInstance>::Node::Node(int32_t indices_offset, int32_t indices_count, const std::vector<ObjectInstance>& object_instances, const std::vector<uint32_t>& indices)
+  : is_leaf(true), indices_offset(indices_offset), indices_count(indices_count)
 {
-  for (uint32_t i : indices)
+  for (uint32_t i = indices_offset; i < indices_offset + indices_count; i++)
   {
-    bounding_box.min = glm::min(bounding_box.min, object_instances[i].get_world_space_bounding_box().min);
-    bounding_box.max = glm::max(bounding_box.max, object_instances[i].get_world_space_bounding_box().max);
+    bounding_box.min = glm::min(bounding_box.min, object_instances[indices[i]].get_world_space_bounding_box().min);
+    bounding_box.max = glm::max(bounding_box.max, object_instances[indices[i]].get_world_space_bounding_box().max);
   }
 }
 
@@ -26,14 +27,15 @@ template<> BVH<Triangle>::BVH(const std::vector<Triangle>& objects, uint32_t thr
   spdlog::debug("Successfully created BLAS with {} nodes", nodes.size());
 }
 
-template<> BVH<Triangle>::Node::Node(const std::vector<uint32_t>& indices, const std::vector<Triangle>& triangles) : is_leaf(true), indices(indices)
+template<> BVH<Triangle>::Node::Node(int32_t indices_offset, int32_t indices_count, const std::vector<Triangle>& triangles, const std::vector<uint32_t>& indices)
+  : is_leaf(true), indices_offset(indices_offset), indices_count(indices_count)
 {
-  for (uint32_t i : indices)
+  for (uint32_t i = indices_offset; i < indices_offset + indices_count; i++)
   {
     for (uint32_t v = 0; v < 3; v++)
     {
-      bounding_box.min = glm::min(bounding_box.min, triangles[i].get_triangle_vertex(v).pos);
-      bounding_box.max = glm::max(bounding_box.max, triangles[i].get_triangle_vertex(v).pos);
+      bounding_box.min = glm::min(bounding_box.min, triangles[indices[i]].get_triangle_vertex(v).pos);
+      bounding_box.max = glm::max(bounding_box.max, triangles[indices[i]].get_triangle_vertex(v).pos);
     }
   }
 }
