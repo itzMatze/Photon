6fc523a000b0723a5fad6ebd3414a909af3e4445
Matthias Bruns <matthiasbruns35@gmail.com>
2024-08-30 21:21:29 +0200

    enable instantiations of the random generator

--- include/util/random_generator.hpp
+++ include/util/random_generator.hpp
@@ -1,6 +1,19 @@
 #pragma once
-#include <limits>
 #include <cstdint>
+#include <limits>
+#include <random>
+
+class RandomGenerator
+{
+public:
+  RandomGenerator(uint32_t seed = 42);
+  float random_float(float lower_bound = 0.0f, float upper_bound = 1.0f);
+  int32_t random_int32(int32_t lower_bound = std::numeric_limits<int32_t>::min(), int32_t upper_bound = std::numeric_limits<int32_t>::max());
+
+private:
+  std::uniform_real_distribution<float> distribution;
+  std::mt19937 generator;
+};
 
 namespace rng
 {
