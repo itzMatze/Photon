0f821a2757b3e91e28da4fb4637463fe913d99e4
Matthias Bruns <matthiasbruns35@gmail.com>
2024-08-31 20:04:19 +0200

    enable requesting the singleton instance of the random generator
    Some function require a random generator to be passed to them to be able to use them concurrently. If those functions are used in a different context it should be possible to use the global random generator instance; Though, usually it should not be necessary.

--- include/util/random_generator.hpp
+++ include/util/random_generator.hpp
@@ -21,5 +21,10 @@ float random_float(float lower_bound = 0.0f, float upper_bound = 1.0f);
 
 // generate random int in [lower_bound, upper_bound - 1]
 int32_t random_int32(int32_t lower_bound = std::numeric_limits<int32_t>::min(), int32_t upper_bound = std::numeric_limits<int32_t>::max());
+
+// the instance is not thread safe
+// this function should not be used most of the time
+// instead create your own instance of the RandomGenerator
+RandomGenerator& get_instance();
 } // namespace rng
 
--- src/util/random_generator.cpp
+++ src/util/random_generator.cpp
@@ -5,11 +5,11 @@
 uint32_t get_seed()
 {
 #if defined(USE_DETERMINISTIC_SEED)
-    return 42;
+  return 42;
 #else
-    std::random_device rd;
-    std::uniform_int_distribution<uint32_t> dis(0, 20000);
-    return dis(rd);
+  std::random_device rd;
+  std::uniform_int_distribution<uint32_t> dis(0, 20000);
+  return dis(rd);
 #endif
 }
 
@@ -18,17 +18,18 @@ RandomGenerator::RandomGenerator(uint32_t seed) : distribution(std::uniform_real
 
 float RandomGenerator::random_float(float lower_bound, float upper_bound)
 {
-    return distribution(generator) * (upper_bound - lower_bound) + lower_bound;
+  return distribution(generator) * (upper_bound - lower_bound) + lower_bound;
 }
 
 int32_t RandomGenerator::random_int32(int32_t lower_bound, int32_t upper_bound)
 {
-    return random_float(lower_bound, upper_bound);
+  return random_float(lower_bound, upper_bound);
 }
 
 namespace rng
 {
 RandomGenerator rg(get_seed());
+
 float random_float(float lower_bound, float upper_bound)
 {
   return random_float(lower_bound, upper_bound);
@@ -38,5 +39,10 @@ float random_float(float lower_bound, float upper_bound)
 int32_t random_int32(int32_t lower_bound, int32_t upper_bound)
 {
   return rg.random_int32(lower_bound, upper_bound);
+}
+
+RandomGenerator& get_instance()
+{
+  return rg;
 }
 } // namespace rng
